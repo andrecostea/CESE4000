d153ff4a71138502ac9734d453439d1e6abeb743
Andreea <M.A.Costea@tudelft.nl>
2025-09-08 10:06:52 +0200

    ecercises

--- lecture-2/overflow/src/main.rs
+++ lecture-2/overflow/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
    let age:u8 = u8::MAX; //255
 
    // 0 to 255 only allowed for u8
-   let x:u8 = au8::MAX +1 ;  //overflow value is 0
+   let x:u8 = u8::MAX +1 ;  //overflow value is 0
    // let x:u8 = age +1 ; // age.wrapping_add(1);   //overflow value is 0
    let y:u8 = age + 2;   //overflow value is 1
 
---
+++ lecture-3/o1-onwership/src/main.rs
@@ -0,0 +1,4 @@
+fn main(){
+ let x = vec![1, 2, 3];
+ println!("Here's your vector {x:?}");
+}
