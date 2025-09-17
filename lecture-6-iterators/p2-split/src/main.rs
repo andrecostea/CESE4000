81c4bffaaefb6fcacc3d61a94117f537637ab341
Andreea <M.A.Costea@tudelft.nl>
2025-09-17 08:39:12 +0200

    split

---
+++ lecture-6-iterators/p2-split/src/main.rs
@@ -0,0 +1,34 @@
+fn closure_example() {
+    let sentence = "The wind is gentle, until it isn't!";
+
+    // Split by different characters
+    let words= sentence.split(|c| c == ',' || c == '!' || c == ' ').collect::<Vec<_>>();
+
+    println!("{:?}", words); // ["The", "wind", "is", "gentle", "until", "it", "isn't"]
+    
+    // Iterate without collecting
+    for word in sentence.split(|c| c == ',' || c == '!' || c == ' ') {
+        println!("{}", word);
+    }
+}
+
+fn main() {
+    let sentence = "The wind is gentle";
+    
+    // Split by space ' '
+    let words: Vec<&str> = sentence.split(' ').collect();
+    println!("{:?}", words); // ["The", "wind", "is", "gentle"]
+
+    let &[w1, w2, w3, w4] = sentence
+                                                            .split(' ')
+                                                            .collect::<Vec<_>>()
+                                                            .as_slice() else {
+        todo!();
+    };
+    
+    // Iterate without collecting
+    for word in sentence.split(' ') {
+        println!("{}", word);
+    }
+
+}
