cbd9024b6bcfeb471870e048cf53dcf517bb4e08
Andreea <andre.costea@gmail.com>
2025-09-18 10:18:07 +0200

    lazy

---
+++ lecture-6-iterators/p3-lazy/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // let data = [1, 2, 3, 4, 5];
+    let data: [u32; 100] = std::array::from_fn(|i| (i + 1) as u32);
+
+    // Create an iterator pipeline
+    let iter = data
+        .iter()
+        .map(|x| {
+            println!("Mapping {}", x);
+            x * x
+        })
+        .filter(|x| {
+            println!("Filtering {}", x);
+            x % 4 == 0
+        });
+
+    println!("--- Before collecting ---");
+
+    // Nothing has happened yet!
+    //let result: Vec<_> = iter.collect();
+    let result: Vec<_> = iter.take(5).collect();
+
+    println!("--- After collecting ---");
+    // println!("Data: {:?}", data);
+    println!("Result: {:?}", result);
+}
